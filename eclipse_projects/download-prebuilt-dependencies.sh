908117b580aba9f9ff24d26c562076f3e7b3e244
Iordan Iordanov <iiordanov@gmail.com>
2018-04-08 13:42:06 -0400

    Updates for Opaque to get prebuilt dependencies as well.

--- eclipse_projects/download-prebuilt-dependencies.sh
+++ eclipse_projects/download-prebuilt-dependencies.sh
@@ -3,11 +3,18 @@
 DIR=$(dirname $0)
 pushd $DIR
 
-DEPVER=2
+DEPVER=3
 
 wget -c https://github.com/iiordanov/remote-desktop-clients/releases/download/dependencies/remote-desktop-clients-libs-${DEPVER}.tar.gz
 
 tar xf remote-desktop-clients-libs-${DEPVER}.tar.gz
+
+rsync -a bVNC/prebuilt/ Opaque/prebuilt/
+
+for d in bVNC Opaque
+do
+  rsync -a ${d}/prebuilt/ ${d}/java
+done
 
 echo "Done downloading and extracting dependencies."
 
