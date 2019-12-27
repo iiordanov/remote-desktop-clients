17248de07202263d25e658ec96743ea15f9f18d2
iordan iordanov <iiordanov@gmail.com>
2019-12-27 18:15:35 -0500

    Initial iOS prototype that uses the libvncserver library.

---
+++ ios/bVNC/bVNC/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  bVNC
+//
+//  Created by iordan iordanov on 2019-12-25.
+//  Copyright © 2019 iordan iordanov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Welcome")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios/bVNC/bVNC/VncBridge.h
@@ -0,0 +1,18 @@
+//
+//  VncBridge.h
+//  bVNC
+//
+//  Created by iordan iordanov on 2019-12-26.
+//  Copyright © 2019 iordan iordanov. All rights reserved.
+//
+
+#ifndef VncBridge_h
+#define VncBridge_h
+
+#include <stdio.h>
+#include "rfb/rfbclient.h"
+
+void connectVnc(void (*callback)(uint8_t *, int fbW, int fbH, int x, int y, int w, int h), char*, char*, char*);
+void (*callback_from_swift)(uint8_t *, int fbW, int fbH, int x, int y, int w, int h);
+
+#endif /* VncBridge_h */
