db9be78d89d950a08f91884dd285413c9c9e2b84
iordan iordanov <iiordanov@gmail.com>
2020-03-03 01:24:38 -0500

    Many fixes for pointer control.

--- ios/bVNC/bVNC/TouchEnabledUIImageView.swift
+++ ios/bVNC/bVNC/TouchEnabledUIImageView.swift
@@ -9,47 +9,6 @@
 import Foundation
 import UIKit
 
-extension UIImageView {
-    private static var _x = [String:CGFloat]()
-    var x:CGFloat {
-        get {
-            let tmpAddress = String(format: "%p", unsafeBitCast(self, to: Int.self))
-            return UIImageView._x[tmpAddress] ?? 0.0
-        }
-        set(newValue) {
-            let tmpAddress = String(format: "%p", unsafeBitCast(self, to: Int.self))
-            UIImageView._x[tmpAddress] = newValue
-        }
-    }
-    
-    func enableGestures() {
-        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan(_:)))
-        panGesture.minimumNumberOfTouches = 2
-        panGesture.maximumNumberOfTouches = 2
-        let pinchGesture = UIPinchGestureRecognizer(target: self, action: #selector(startZooming(_:)))
-        isUserInteractionEnabled = true
-        addGestureRecognizer(pinchGesture)
-        addGestureRecognizer(panGesture)
-    }
-    
-    @objc private func handlePan(_ sender: UIPanGestureRecognizer) {
-        let scaleX = sender.view!.transform.a
-        let scaleY = sender.view!.transform.d
-        let translation = sender.translation(in: sender.view)
-        if let view = sender.view {
-            view.center = CGPoint(x: (view.center.x + scaleX*translation.x), y: (view.center.y + scaleY*translation.y))
-        }
-        sender.setTranslation(CGPoint.zero, in: sender.view)
-    }
-    
-    @objc private func startZooming(_ sender: UIPinchGestureRecognizer) {
-        let transformResult = sender.view?.transform.scaledBy(x: sender.scale, y: sender.scale)
-        guard let newTransform = transformResult, newTransform.a > 1, newTransform.d > 1 else { return }
-        sender.view?.transform = newTransform
-        sender.scale = 1
-  }
-}
-
 class TouchEnabledUIImageView: UIImageView {
     var fingers = [UITouch?](repeating: nil, count:5)
     var width: CGFloat = 0.0
@@ -65,141 +24,248 @@ class TouchEnabledUIImageView: UIImageView {
     var secondDown: Bool = false
     var thirdDown: Bool = false
     var point: CGPoint = CGPoint(x: 0, y: 0)
+    let lock = NSLock()
+    var panGesture: UIPanGestureRecognizer?
+    var pinchGesture: UIPinchGestureRecognizer?
+    var moveEventsSinceFingerDown = 0
 
+    func initialize() {
+        isMultipleTouchEnabled = true
+        self.width = self.frame.width
+        self.height = self.frame.height
+        panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan(_:)))
+        pinchGesture = UIPinchGestureRecognizer(target: self, action: #selector(handleZooming(_:)))
+        panGesture?.minimumNumberOfTouches = 2
+        panGesture?.maximumNumberOfTouches = 2
+
+    }
+    
     override init(image: UIImage?) {
         super.init(image: image)
-        isMultipleTouchEnabled = true
-        self.width = self.frame.width
-        self.height = self.frame.height
+        initialize()
     }
-
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        isMultipleTouchEnabled = true
-        self.width = self.frame.width
-        self.height = self.frame.height
+        initialize()
     }
-
+    
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        isMultipleTouchEnabled = true
+        initialize()
     }
     
     func enableTouch() {
         touchEnabled = true
     }
-
+    
     func disableTouch() {
         touchEnabled = false
     }
     
-    func setViewParameters(touch: UITouch, touchView: UIView) -> Bool {
+    func isOutsideImageBoundaries(touch: UITouch, touchView: UIView) -> Bool {
         if (!touch.view!.isKind(of: UIImageView.self)) {
             return false
         }
+        return true
+    }
+    
+    func setViewParameters(touch: UITouch, touchView: UIView) {
         self.width = touchView.frame.width
         self.height = touchView.frame.height
         self.point = touch.location(in: touchView)
         self.viewTransform = touchView.transform
         self.newX = self.point.x*viewTransform.a
         self.newY = self.point.y*viewTransform.d
-        return true
     }
     
-    func sendPointerEvent(action: String, index: Int, touch: UITouch, forceSend: Bool, firstDown: Bool, secondDown: Bool, thirdDown: Bool) {
+    func sendPointerEvent(action: String, index: Int, touch: UITouch, moving: Bool, firstDown: Bool, secondDown: Bool, thirdDown: Bool) {
         Background {
-            //print(#function)
-            if (!firstDown && !secondDown && !thirdDown &&
-                !self.firstDown && !self.secondDown && !self.thirdDown) {
-                print("No mouse button is down but we were told to send button up event, returning.")
-                return
-            }
-            self.firstDown = firstDown
-            self.secondDown = secondDown
-            self.thirdDown = thirdDown
-            let currentTime = CACurrentMediaTime()
-            print(action + ": \(index+1): x=\(self.point.x), y=\(self.point.y), tx=\(self.viewTransform.tx), ty=\(self.viewTransform.ty), a=\(self.viewTransform.a), b=\(self.viewTransform.b), c=\(self.viewTransform.c), d=\(self.viewTransform.d)")
-            //print("currentTime - lastTime: " + String(abs(self.lastTime - currentTime)) + " last time: " + String(self.lastTime), " forceSend: " + String(forceSend) + " touchEnabled: " + String(self.touchEnabled))
-            if (self.touchEnabled && (forceSend || abs(self.lastTime - currentTime) > 0.1 && abs(self.lastX - self.newX) > 1.0 && abs(self.lastX - self.newX) > 1.0)) {
-                self.lastX = self.newX
-                self.lastY = self.newY
-                if(forceSend) {
-                    Thread.sleep(forTimeInterval: 0.2)
+            self.lock.lock()
+            if (self.touchEnabled) {
+                if !moving ||
+                    abs(self.lastX - self.newX) > 15.0 || abs(self.lastY - self.newY) > 15.0 {
+                    print ("Not moving or moved far enough, sending event.")
+                    sendPointerEventToServer(Int32(self.width), Int32(self.height), Int32(self.newX), Int32(self.newY), firstDown, secondDown, thirdDown)
+                    self.lastX = self.newX
+                    self.lastY = self.newY
+                } else {
+                    print ("Moving and not moved far enough, discarding event.")
                 }
-                self.lastTime = currentTime
-                sendPointerEventToServer(Int32(self.width), Int32(self.height), Int32(self.newX), Int32(self.newY), firstDown, secondDown, thirdDown)
             }
+            self.lock.unlock()
         }
     }
-
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesBegan(touches, with: event)
+        /*
+        if (touches.count == 2) {
+            print("Two fingers down, so ignore to allow pinch zooming and panning unhindered.")
+            return
+        }*/
         for touch in touches {
             if let touchView = touch.view {
-                if !setViewParameters(touch: touch, touchView: touchView) {
+                if !isOutsideImageBoundaries(touch: touch, touchView: touchView) {
                     print("Touch is outside image, ignoring.")
                     continue
                 }
             } else {
                 print("Could not unwrap touch.view, sending event at last coordinates.")
             }
-
+            
             for (index, finger)  in self.fingers.enumerated() {
                 if finger == nil {
                     self.fingers[index] = touch
-                    self.sendPointerEvent(action: "finger down", index: index, touch: touch, forceSend: false, firstDown: true, secondDown: false, thirdDown: false)
+                    if index == 0 {
+                        moveEventsSinceFingerDown = 0
+                        print("ONE FINGER Detected, marking this a left-click")
+                        self.firstDown = true
+                        self.secondDown = false
+                        self.thirdDown = false
+                        // Record location only for first index
+                        if let touchView = touch.view {
+                            self.setViewParameters(touch: touch, touchView: touchView)
+                        }
+                    }
+                    if index == 1 {
+                        print("TWO FINGERS Detected, marking this a right-click")
+                        self.firstDown = false
+                        self.secondDown = false
+                        self.thirdDown = true
+                    }
+                    if index == 2 {
+                        print("THREE FINGERS Detected, marking this a middle-click")
+                        self.firstDown = false
+                        self.secondDown = true
+                        self.thirdDown = false
+                    }
                     break
                 }
             }
         }
     }
-
+    
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesMoved(touches, with: event)
         for touch in touches {
             if let touchView = touch.view {
-                if !setViewParameters(touch: touch, touchView: touchView) {
+                if !isOutsideImageBoundaries(touch: touch, touchView: touchView) {
                     print("Touch is outside image, ignoring.")
                     continue
                 }
             } else {
                 print("Could not unwrap touch.view, sending event at last coordinates.")
             }
-
-            for (index,finger) in self.fingers.enumerated() {
+            
+            for (index, finger) in self.fingers.enumerated() {
                 if let finger = finger, finger == touch {
-                    self.sendPointerEvent(action: "finger moved", index: index, touch: touch, forceSend: false, firstDown: true, secondDown: false, thirdDown: false)
+                    if index == 0 {
+                        // Record location only for first index
+                        if let touchView = touch.view {
+                            self.setViewParameters(touch: touch, touchView: touchView)
+                        }
+                        if moveEventsSinceFingerDown > 1 {
+                            self.sendPointerEvent(action: "finger moved", index: index, touch: touch, moving: true, firstDown: self.firstDown, secondDown: self.secondDown, thirdDown: self.thirdDown)
+                        } else {
+                            print("Discarding some events")
+                            moveEventsSinceFingerDown += 1
+                        }
+                    }
                     break
                 }
             }
         }
     }
-
+    
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesEnded(touches, with: event)
+        if (!firstDown && !secondDown && !thirdDown) {
+            print("No buttons are indicated to be down, ignoring.")
+            return
+        }
+
         for touch in touches {
             if let touchView = touch.view {
-                if !setViewParameters(touch: touch, touchView: touchView) {
+                if !isOutsideImageBoundaries(touch: touch, touchView: touchView) {
                     print("Touch is outside image, ignoring.")
                     continue
                 }
             } else {
                 print("Could not unwrap touch.view, sending event at last coordinates.")
             }
-
-            for (index,finger) in self.fingers.enumerated() {
+            
+            for (index, finger) in self.fingers.enumerated() {
                 if let finger = finger, finger == touch {
                     self.fingers[index] = nil
-                    self.sendPointerEvent(action: "finger lifted", index: index, touch: touch, forceSend: true, firstDown: false, secondDown: false, thirdDown: false)
+                    if (index == 0) {
+                        if (self.panGesture?.state == .began || self.pinchGesture?.state == .began) {
+                            print("Is panning or zooming and first finger lifted, not sending mouse events.")
+                        } else {
+                            print("Not panning or zooming and first finger lifted, sending mouse events.")
+                            self.sendPointerEvent(action: "finger lifted", index: index, touch: touch, moving: false, firstDown: self.firstDown, secondDown: self.secondDown, thirdDown: self.thirdDown)
+                            self.firstDown = false
+                            self.secondDown = false
+                            self.thirdDown = false
+                            self.sendPointerEvent(action: "finger lifted", index: index, touch: touch, moving: false, firstDown: self.firstDown, secondDown: self.secondDown, thirdDown: self.thirdDown)
+                        }
+                    } else {
+                        print("Fingers other than first lifted, not sending mouse events.")
+                    }
                     break
                 }
             }
         }
     }
-
+    
     override func touchesCancelled(_ touches: Set<UITouch>?, with event: UIEvent?) {
         super.touchesCancelled(touches!, with: event)
         guard let touches = touches else { return }
         self.touchesEnded(touches, with: event)
     }
+    
+    func enableGestures() {
+        isUserInteractionEnabled = true
+        if let pinchGesture = pinchGesture { addGestureRecognizer(pinchGesture) }
+        if let panGesture = panGesture { addGestureRecognizer(panGesture) }
+    }
+    
+    @objc private func handlePan(_ sender: UIPanGestureRecognizer) {
+        let translation = sender.translation(in: sender.view)
+
+        if let view = sender.view {
+            let scaleX = sender.view!.transform.a
+            let scaleY = sender.view!.transform.d
+
+            var newCenterX = view.center.x + scaleX*translation.x
+            var newCenterY = view.center.y + scaleY*translation.y
+            let scaledWidth = sender.view!.frame.width/scaleX
+            let scaledHeight = sender.view!.frame.height/scaleY
+            if sender.view!.frame.minX/scaleX >= 20 { newCenterX = view.center.x - 10 }
+            if sender.view!.frame.minY/scaleY >= 20 { newCenterY = view.center.y - 10 }
+            if sender.view!.frame.minX/scaleX <= -20 - (scaleX-1.0)*scaledWidth/scaleX { newCenterX = view.center.x + 10 }
+            if sender.view!.frame.minY/scaleY <= -20 - (scaleY-1.0)*scaledHeight/scaleY { newCenterY = view.center.y + 10 }
+            view.center = CGPoint(x: newCenterX, y: newCenterY)
+        }
+        sender.setTranslation(CGPoint.zero, in: sender.view)
+    }
+    
+    @objc private func handleZooming(_ sender: UIPinchGestureRecognizer) {
+        let scale = sender.scale
+        let transformResult = sender.view?.transform.scaledBy(x: sender.scale, y: sender.scale)
+        guard let newTransform = transformResult, newTransform.a > 1, newTransform.d > 1 else { return }
+
+        if let view = sender.view {
+            let scaledWidth = sender.view!.frame.width/scale
+            let scaledHeight = sender.view!.frame.height/scale
+            if view.center.x/scale < -20 { view.center.x = -20*scale }
+            if view.center.y/scale < -20 { view.center.y = -20*scale }
+            if view.center.x/scale > scaledWidth/2 + 20 { view.center.x = (scaledWidth/2 + 20)*scale }
+            if view.center.y/scale > scaledHeight/2 + 20 { view.center.y = (scaledHeight/2 + 20)*scale }
+        }
+        sender.view?.transform = newTransform
+        sender.scale = 1
+        //print("Frame: \(sender.view!.frame)")
+    }
+    
 }
