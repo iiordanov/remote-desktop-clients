684ff32d1d918c99442efc02e2213847a9e057d1
iordan iordanov <iiordanov@gmail.com>
2020-03-20 16:16:17 -0400

    Multiple saved connections handling.

--- ios/bVNC/bVNC/StateKeeper.swift
+++ ios/bVNC/bVNC/StateKeeper.swift
@@ -12,7 +12,9 @@ import SwiftUI
 
 class StateKeeper: ObservableObject {
     let objectWillChange = PassthroughSubject<StateKeeper, Never>()
-    var currentConnection: [String: String]
+    var selectedConnection: [String: String]
+    var connections: [Dictionary<String, String>]
+    var connectionIndex: Int
     var settings = UserDefaults.standard
     var scene: UIScene?
     var window: UIWindow?
@@ -21,10 +23,12 @@ class StateKeeper: ObservableObject {
     
     init() {
         // Load settings for current connection
-        currentConnection = self.settings.dictionary(forKey: "selectedConnection") as? [String:String] ?? [:]
+        selectedConnection = self.settings.dictionary(forKey: "selectedConnection") as? [String:String] ?? [:]
+        connectionIndex = self.settings.integer(forKey: "connectionIndex")
+        connections = self.settings.array(forKey: "connections") as? [Dictionary<String, String>] ?? []
     }
     
-    var currentPage: String = "page1" {
+    var currentPage: String = "page0" {
         didSet {
             self.objectWillChange.send(self)
         }
@@ -42,20 +46,81 @@ class StateKeeper: ObservableObject {
         self.imageView = imageView
     }
 
-    func connect() {
-        // Save settings for current connection
-        self.settings.set(self.currentConnection, forKey: "selectedConnection")
+    func connect(index: Int) {
+        print("Connecting and navigating to the connection screen")
+        self.connectionIndex = index
+        self.selectedConnection = self.connections[index]
         self.vncSession = VncSession(scene: self.scene!, stateKeeper: self, window: self.window!)
-        self.vncSession!.connect(currentConnection: self.currentConnection)
+        self.vncSession!.connect(currentConnection: self.selectedConnection)
         self.currentPage = "page4"
     }
 
     @objc func disconnect() {
+        print("Disconnecting and navigating to the disconnection screen")
         imageView?.disableTouch()
         self.vncSession?.disconnect()
-        // TODO: Show a spinner instead of going to the connection screen and allow the backend to indicate when the disconnection is completed
-        //let contentView = ContentView(stateKeeper: self)
-        //window!.rootViewController = UIHostingController(rootView: contentView)
         self.currentPage = "page3"
+    }
+    
+    func addNew() {
+        print("Adding new connection and navigating to connection setup screen")
+        self.connectionIndex = -1
+        self.selectedConnection = [:]
+        self.currentPage = "page1"
+    }
+
+    func edit(index: Int) {
+        print("Editing connection at index \(index)")
+        self.connectionIndex = index
+        self.selectedConnection = connections[index]
+        print("Navigating to setup screen for connection at index \(self.connectionIndex)")
+        self.currentPage = "page1"
+    }
+
+    func deleteCurrent() {
+        print("Deleting connection at index \(self.connectionIndex) and navigating to list of connections screen")
+        // Do something only if we were not adding a new connection.
+        if connectionIndex >= 0 {
+            print("Deleting connection with index \(connectionIndex)")
+            self.connections.remove(at: self.connectionIndex)
+            self.connectionIndex = 0
+            if self.connections.count > 0 {
+                self.selectedConnection = self.connections[connectionIndex]
+            } else {
+                self.selectedConnection = [:]
+            }
+            self.settings.set(self.selectedConnection, forKey: "selectedConnection")
+            self.settings.set(self.connectionIndex, forKey: "connectionIndex")
+            self.settings.set(self.connections, forKey: "connections")
+        } else {
+            print("We were adding a new connection, so not deleting anything")
+        }
+        self.currentPage = "page0"
+    }
+    
+    func saveSettings() {
+        print("Saving settings")
+        self.settings.set(self.selectedConnection, forKey: "selectedConnection")
+        self.settings.set(self.connectionIndex, forKey: "connectionIndex")
+        self.settings.set(self.connections, forKey: "connections")
+    }
+    
+    func saveNewConnection(connection: [String: String]) {
+        self.selectedConnection = connection
+        // Negative index indicates we are adding a connection, otherwise we are editing one.
+        if (connectionIndex < 0) {
+            print("Saving a new connection and navigating to list of connections")
+            self.connectionIndex = self.connections.count - 1
+            self.connections.append(connection)
+        } else {
+            print("Saving a connection at index \(self.connectionIndex) and navigating to list of connections")
+            self.connections[connectionIndex] = connection
+        }
+        self.saveSettings()
+        self.currentPage = "page0"
+    }
+    
+    func showConnections() {
+        self.currentPage = "page0"
     }
 }
