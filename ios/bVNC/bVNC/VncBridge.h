a566ca81c0bdc5d33f90da20ed05fb6ae092c0f9
iordan iordanov <iiordanov@gmail.com>
2020-03-24 01:51:43 -0400

    Implemented SSH certificate acceptance functionality. Bugfix for remote desktop framebuffer images remaining in the background when remote framebuffer is resized.

--- ios/bVNC/bVNC/VncBridge.h
+++ ios/bVNC/bVNC/VncBridge.h
@@ -18,7 +18,7 @@ void connectVnc(void (*fb_update_callback)(uint8_t *, int fbW, int fbH, int x, i
                 void (*fb_resize_callback)(int fbW, int fbH),
                 void (*fail_callback)(int8_t *),
                 void (*cl_log_callback)(int8_t *),
-                int (*y_n_callback)(int8_t *, int8_t *, int8_t *, int8_t *),
+                int (*y_n_callback)(int8_t *, int8_t *, int8_t *, int8_t *, int8_t *),
                 char* addr, char* user, char* password, char* ca_path);
 
 void disconnectVnc(void);
@@ -33,5 +33,6 @@ void sendUniDirectionalKeyEventWithKeySym(int sym, bool down);
 void sendPointerEventToServer(int totalX, int totalY, int x, int y, bool firstDown, bool secondDown, bool thirdDown, bool scrollUp, bool scrollDown);
 void checkForError(rfbBool res);
 void cleanup(char* message, rfbClient *client);
+void rfb_client_cleanup();
 
 #endif /* VncBridge_h */
