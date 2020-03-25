071a752593a5a7212711d9addc6a971e45868fca
iordan iordanov <iiordanov@gmail.com>
2020-03-24 21:21:54 -0400

    Implemented rate limiting for rendering of new UIImages as a result of pixel updates from the server based on app CPU usage to keep UI responsive.

---
+++ ios/bVNC/bVNC/Utilities.h
@@ -0,0 +1,14 @@
+//
+//  Utilities.h
+//  bVNC
+//
+//  Created by iordan iordanov on 2020-03-24.
+//  Copyright © 2020 iordan iordanov. All rights reserved.
+//
+
+#ifndef Utilities_h
+#define Utilities_h
+
+Float64 cpuUsage();
+
+#endif /* Utilities_h */
