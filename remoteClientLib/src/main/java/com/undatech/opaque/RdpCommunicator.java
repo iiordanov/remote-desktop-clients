fc7c93b42fa817f14b6c08112ee86541c5fa2fe2
Iordan Iordanov <iiordanov@gmail.com>
2020-08-14 19:06:46 -0400

    Upgraded to stable-2.0 branch of freerdp but had to disable openh264 because it breaks the build with an error about the library file format.

--- remoteClientLib/src/main/java/com/undatech/opaque/RdpCommunicator.java
+++ remoteClientLib/src/main/java/com/undatech/opaque/RdpCommunicator.java
@@ -263,7 +263,10 @@ public class RdpCommunicator implements RfbConnectable, RdpKeyboardMapper.KeyPro
         android.util.Log.e(TAG, "Unicode character: " + unicodeKey);
         sendModifierKeys(true);
         try { Thread.sleep(5); } catch (InterruptedException e) {}
-        LibFreeRDP.sendUnicodeKeyEvent(session.getInstance(), unicodeKey);
+        LibFreeRDP.sendUnicodeKeyEvent(session.getInstance(), unicodeKey, true);
+        try { Thread.sleep(5); } catch (InterruptedException e) {}
+        LibFreeRDP.sendUnicodeKeyEvent(session.getInstance(), unicodeKey, false);
+        try { Thread.sleep(5); } catch (InterruptedException e) {}
         sendModifierKeys(false);
     }
 
