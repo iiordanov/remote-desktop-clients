d6fcefcbc81920bfedf0571cf833ad7a65fe21d3
Iordan Iordanov <iiordanov@gmail.com>
2021-05-09 13:14:26 -0400

    Adding copyright notices and license information.

--- ios/bVNC/bVNC/SystemMonitor.m
+++ ios/bVNC/bVNC/SystemMonitor.m
@@ -1,10 +1,22 @@
-//
-//  SystemMonitor.m
-//  bVNC
-//
-//  Created by iordan iordanov on 2020-03-24.
-//  Copyright © 2020 iordan iordanov. All rights reserved.
-//
+/**
+ * Copyright (C) 2021- Morpheusly Inc. All rights reserved.
+ *
+ * This is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this software; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307,
+ * USA.
+ */
+
 // From https://stackoverflow.com/questions/43866416/get-detailed-ios-cpu-usage-with-different-states
 
 #import <Foundation/Foundation.h>
--- ios/bVNC/bVNC/VncBridge.c
+++ ios/bVNC/bVNC/VncBridge.c
@@ -1,10 +1,21 @@
-//
-//  VncBridge.c
-//  bVNC
-//
-//  Created by iordan iordanov on 2019-12-26.
-//  Copyright © 2019 iordan iordanov. All rights reserved.
-//
+/**
+ * Copyright (C) 2021- Morpheusly Inc. All rights reserved.
+ *
+ * This is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this software; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307,
+ * USA.
+ */
 
 #include <pthread/pthread.h>
 #include "VncBridge.h"
